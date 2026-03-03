383748413cf127edb65e6695ade05dbd6ff7e2ca
DanielGonzalezBaltazar <dannyo.gonzalez2502@gmail.com>
2026-03-02 21:16:55 -0600

    text

--- ClaseJs/Sscrip.js
+++ ClaseJs/Sscrip.js
@@ -1,4 +1,16 @@
-function btn_ingresar(){
-    location.href=pag2.html;
+function btn_ingresa(){
+    location.href="pag2.htm";
+}
+function btn(){
+
+}
+function btn(){
+    
+}
+function btn(){
+    
+}
+function btn(){
+    
 }
 //Para mandar correos   /^[a-zA-Z0-9._%+-]+@[cecytemorelos]+\.[a-zA-Z]+\.[a-z]$/
