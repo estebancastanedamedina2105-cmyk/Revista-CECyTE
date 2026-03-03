855da185dfd73826a6964c4af66f51d82cda4147
Bastixs <sebas05.delarosa@gmail.com>
2026-03-02 22:13:51 -0600

    me estrese

--- Chatbot/Chatbot.py
+++ Chatbot/Chatbot.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template , request
 
 app = Flask(__name__)
 
 
-@app.route("/SitioWeb")
+@app.route("/")
 def SitioWeb():
     return "¡Hola desde el chatbot!"
+
+
+@app.route("/chatbot", methods=["POST"])
+def chatbot():
+    user_message = request.form ['message']
+    bot_reponse  process_text(user_message)
+    
 
 
 if __name__ == "__main__":
