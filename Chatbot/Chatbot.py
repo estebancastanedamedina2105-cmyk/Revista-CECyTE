c19bba05f52bbfb49c9cf95058021b04b1c8d436
Bastixs <sebas05.delarosa@gmail.com>
2026-03-03 11:09:48 -0600

    Implementa chatbot y nuevas secciones: actualiza rutas, agrega funcionalidad de chatbot y crea nuevas páginas para Cafetería y Perfil.

--- Chatbot/Chatbot.py
+++ Chatbot/Chatbot.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template , request
+from flask import Flask, render_template, request
+from chatbot import process_text
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def SitioWeb():
-    return "¡Hola desde el chatbot!"
+    return render_template("1-Anuncios.html")
 
 
-@app.route("/chatbot", methods=["POST"])
+@app.route("/chatbot", methods=["POST"])  # type: ignore
 def chatbot():
-    user_message = request.form ['message']
-    bot_reponse  process_text(user_message)
-    
+    user_message = request.form["message"]
+    bot_reponse = process_text(user_message)
+    return jsonify({"response": bot_reponse})  # type: ignore
 
 
 if __name__ == "__main__":
