0a3b8f12aac3f83f0aa2bdce9f0b653bf57df4ca
DanielGonzalezBaltazar <dannyo.gonzalez2502@gmail.com>
2026-03-03 22:31:06 -0600

    menu, text

---
+++ Clase Js/Sscrip.js
@@ -0,0 +1,16 @@
+function btn_oscuro(){
+    window.location.href = "5-Login.html";
+}
+console.log("Hola_Mundo");
+
+function toggleChatbot() {
+    var chatBody = document.getElementById("chatbot-body");
+    chatBod.style.display = chatBody.style.display === "none" ;
+    }
+function sednMensaje() {
+    var input = document.getElementById("chat-input");
+    var mensaje = input.value;
+    input.value = "";
+    fetch("/Chatbot")
+}
+//Para mandar correos   /^[a-zA-Z0-9._%+-]+@[cecytemorelos]+\.[edu]+\.[mx]$/
