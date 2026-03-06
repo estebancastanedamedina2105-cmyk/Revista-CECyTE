abc50a35489219a108b67319bf6b919d7f4c0f23
DanielGonzalezBaltazar <dannyo.gonzalez2502@gmail.com>
2026-03-06 00:20:40 -0600

    feat: Enhance user interaction with like button and add image upload functionality

--- Clase Js/chismografo.js
+++ Clase Js/chismografo.js
@@ -2,36 +2,36 @@ document.addEventListener("DOMContentLoaded", () => {
   const likeBtn = document.getElementById("likeBtn");
   const likeCountSpan = document.getElementById("likeCount");
 
-  // 1. Cargar el contador y el estado del voto
+    // 1. Cargar el contador y el estado del voto
   let likes = parseInt(localStorage.getItem("likeCount")) || 0;
   const hasLiked = localStorage.getItem("hasLiked") === "true";
 
-  likeCountSpan.innerText = likes;
-
-  // 2. Si ya votó anteriormente, deshabilitar el botón de inmediato
-  if (hasLiked) {
-    bloquearBoton(likeBtn);
-  }
-
-  likeBtn.addEventListener("click", () => {
-    // 3. Doble verificación por seguridad
-    if (localStorage.getItem("hasLiked") === "true") return;
-
-    likes++;
     likeCountSpan.innerText = likes;
 
-    // 4. Guardar el nuevo total y marcar que ya votó
+    // 2. Si ya votó anteriormente, deshabilitar el botón de inmediato
+    if (hasLiked) {
+        bloquearBoton(likeBtn);
+    }
+
+  likeBtn.addEventListener("click", () => {
+        // 3. Doble verificación por seguridad
+    if (localStorage.getItem("hasLiked") === "true") return;
+
+        likes++;
+        likeCountSpan.innerText = likes;
+
+        // 4. Guardar el nuevo total y marcar que ya votó
     localStorage.setItem("likeCount", likes);
     localStorage.setItem("hasLiked", "true");
 
-    bloquearBoton(likeBtn);
-  });
+        bloquearBoton(likeBtn);
+    });
 });
 
 function bloquearBoton(boton) {
-  boton.disabled = true; // El usuario no puede volver a hacer click
-  boton.innerText = "Ya diste like";
-  boton.style.opacity = "0.5"; // Feedback visual opcional
+    boton.disabled = true; // El usuario no puede volver a hacer click
+    boton.innerText = "Ya diste like";
+    boton.style.opacity = "0.5"; // Feedback visual opcional
 }
 
 document.getElementById("postBtn").addEventListener("click", function () {
--- Clase Js/login.js
+++ Clase Js/login.js
@@ -1,2 +1,17 @@
 console.log("Bienvenido a ingresar");
+
+
+function generarCodigo() {
+  const codigo = Math.floor(1000 + Math.random() * 9000);
+  return codigo.toString(); // Convierte a cadena por si se requiere manipulación
+}
+
+console.log("Código generado: " + generarCodigo());
+    
+function codigo () {
+    
+}
+function entrarCode() {
+    window.location.href = "../templates/1_Anuncios.html";
+}
 //Para mandar correos   /^[a-zA-Z0-9._%+-]+@[cecytemorelos]+\.[edu]+\.[mx]$/
