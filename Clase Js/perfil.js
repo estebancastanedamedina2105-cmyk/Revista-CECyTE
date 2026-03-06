8b3cc1ff5f6c971f2e85b79cecb39940ec9846a7
DanielGonzalezBaltazar <dannyo.gonzalez2502@gmail.com>
2026-03-06 00:27:59 -0600

    refactor: Remove unused image upload functionality from perfil.js

--- Clase Js/perfil.js
+++
@@ -1,5 +0,0 @@
-const image = document.querySelector('img');
-const input = document.querySelector('input');
-input.addEventListener('change', (e) => {
-    image.src =URL.createObjectURL(e.target.files[0]);
-});
